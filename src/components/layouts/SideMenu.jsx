a85fa1da41ff3ae08777598e5524a612fd9ceaab
maxxi <129664117+maxxi02@users.noreply.github.com>
2025-07-21 22:55:24 +0800

    fixed img

--- src/components/layouts/SideMenu.jsx
+++ src/components/layouts/SideMenu.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { SIDE_MENU_DATA as data } from "../../utils/data.js";
 import UserContext from "../../context/user-context.jsx";
@@ -7,7 +7,7 @@ import CharAvatar from "../Cards/CharAvatar.jsx";
 const SideMenu = ({ activeMenu }) => {
   const { user, clearUser } = useContext(UserContext);
   const navigate = useNavigate();
-
+  const [imageError, setImageError] = useState(false);
   const handleClick = (route) => {
     if (route === "logout") {
       handleLogout();
@@ -24,11 +24,13 @@ const SideMenu = ({ activeMenu }) => {
   return (
     <div className="w-64 h-[calc(100vh-61px)] bg-white border-r border-gray-200/50 p-5 sticky top-[61px] z-20">
       <div className="flex flex-col items-center justify-center gap-3 mt-3 mb-3">
-        {user?.profileImageUrl ? (
+        {user?.profileImageUrl && !imageError ? (
           <img
-            src={user?.profileImageUrl || ""}
+            src={user.profileImageUrl || ""}
             alt="Profile image"
             className="w-20 h-20 bg-slate-400 rounded-full object-cover"
+            onError={() => setImageError(true)}
+            onLoad={() => setImageError(false)}
           />
         ) : (
           <CharAvatar
@@ -45,7 +47,7 @@ const SideMenu = ({ activeMenu }) => {
       {data.map((item, index) => (
         <button
           key={`menu_${index}`}
-          className={`w-full flex items-center gap-4 text-[15px ${activeMenu == item.label ? "text-white bg-primary" : ""} py-3 px-6 rounded-lg mb-3`}
+          className={`w-full flex items-center gap-4 text-[15px] ${activeMenu == item.label ? "text-white bg-primary" : ""} py-3 px-6 rounded-lg mb-3`}
           onClick={() => handleClick(item.path)}
         >
           <item.icon className="text-xl" />
